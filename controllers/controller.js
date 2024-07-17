50f04f21f64591c911ab579bdf7e5768d3687270
Donny Pratama <rizkydonny0@gmail.com>
2024-07-17 22:05:01 +0700

    selsai routing

---
+++ controllers/controller.js
@@ -0,0 +1,128 @@
+const {Meme, Category} =  require("../models")
+
+
+class Controller{
+    static async LandingPage(req,res){
+        try {
+            req.render('landingpage')
+        } catch (error) {
+            res.send(error)          
+        }
+    }   
+    static async getSignUp(req,res){
+        try {
+           
+            
+        } catch (error) {
+            res.send(error)
+            
+        }
+    }
+    static async postSignUp(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async getSignIn(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async postSignIn(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async getProfile(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async userDetail(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async getEditUser(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async postEditUser(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+    static async home(req,res){
+        try {
+            res.render('home')
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+    static async getListCompany(req,res){
+        try {
+            let data = await Company.findAll()
+            res.render('company', {data})
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async buyStock(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async sellStock(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+
+    static async deleteCompany(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+    static async getSignOut(req,res){
+        try {
+            
+        } catch (error) {
+            req.send(error)         
+        }
+    }
+}
+
+
+
+
+
+module.exports = Controller
---
+++ routers/index.js
@@ -0,0 +1,32 @@
+const router = require('express').Router()
+const Controller = require('../controllers/controller')
+
+
+
+router.get('/', Controller.LandingPage)
+router.get('/signup', Controller.getSignUp)
+router.post('/signup', Controller.postSignUp)
+router.get('/signin', Controller.getSignIn)
+router.post('/signin', Controller.postSignIn)
+router.get('/userprofile', Controller.getProfile)
+router.get('/userprofile/:id/detail', Controller.userDetail)
+router.get('/userprofile/:id/edit', Controller.getEditUser)
+router.post('/userprofile/:id/edit', Controller.postEditUser)
+router.get('/home', Controller.home) 
+router.get('/company', Controller.getListCompany) 
+router.get('/company/:id/buy', Controller.buyStock) 
+router.get('/company/:id/sell', Controller.sellStock) 
+router.get('/company/:id/delete', Controller.deleteCompany) 
+router.get('/signout', Controller.getSignOut)
+
+
+
+
+
+
+
+
+
+
+module.exports = router
+
